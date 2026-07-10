0d9b0fedbc1c591b525a50f68dd8ec08310bcd71
Tonyus-dev <bigotonyus@gmail.com>
2026-07-09 22:52:40 -0300

    chore: generate routeTree for kuan-yin revisao

--- src/routes/_authenticated/kuan-yin.revisao.tsx
+++ src/routes/_authenticated/kuan-yin.revisao.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute } from "@tanstack/react-start";
+import { createFileRoute } from "@tanstack/react-router";
+
 import { useServerFn } from "@tanstack/react-start";
 import { getPendingReviews, resolveReviewAction } from "@/lib/kuanyin-review.functions";
 import { useEffect, useState } from "react";
